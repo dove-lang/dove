32fc6a03a97f354d0021a88bd1b14e207cc87239
Peter Ke <peterke0911@gmail.com>
2020-06-18 21:19:23 -0700

    Update token

--- src/token.rs
+++ src/token.rs
@@ -1,12 +1,13 @@
+#[derive(Clone)]
 pub struct Token {
     pub token_type: TokenType,
     pub lexeme: String,
-    pub literal: Literals,
+    pub literal: Option<Literals>,
     pub line: usize,
 }
 
 impl Token {
-    pub fn new(token_type: TokenType, lexeme: String, literal: Literals, line: usize) -> Token {
+    pub fn new(token_type: TokenType, lexeme: String, literal: Option<Literals>, line: usize) -> Token {
         Token {
             token_type,
             lexeme,
@@ -22,7 +23,7 @@ impl Token {
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 #[allow(non_camel_case_types)]
 pub enum TokenType {
     // Single-character tokens.
@@ -30,6 +31,7 @@ pub enum TokenType {
     COMMA, DOT, MINUS, NEWLINE, PLUS, SLASH, STAR,
 
     // One or two character tokens.
+    BACKSLASH,
     BANG, BANG_EQUAL,
     EQUAL, EQUAL_EQUAL,
     GREATER, GREATER_EQUAL,
@@ -39,17 +41,17 @@ pub enum TokenType {
     IDENTIFIER, STRING, NUMBER,
 
     // Keywords.
-    AND, BREAK, CLASS, CONTINUE, ELSE, FALSE, FUN, FOR, FROM, IN, IF, LET, NIL, NOT, OR,
-    PRINT, RETURN, SUPER, SELF, TRUE, WHILE,
+    AND, ARRAY, BREAK, CLASS, CONTINUE, DICT, ELSE, FALSE, FUN, FOR, FROM, IN, IF, LET, NIL, NOT, OR,
+    PRINT, RETURN, SUPER, SELF, TRUE, TUPLE, WHILE,
 
     // End of file.
     EOF
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Literals {
-    STRING(String),
-    NUMBER(f64),
-    BOOLEAN(bool),
-    NULL,
+    String(String),
+    Number(f64),
+    Boolean(bool),
+    Nil,
 }
