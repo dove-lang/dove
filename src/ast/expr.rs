0b0444ef93301d61f2463eb8d6e84e07ad8d5ed8
Qiwen Hua <qiwen.hua@mail.utoronto.ca>
2020-06-20 14:43:01 -0700

    Update TokenTypes, Expr, Stmt

--- src/ast/expr.rs
+++ src/ast/expr.rs
@@ -3,14 +3,18 @@ use crate::ast::Stmt;
 
 #[derive(Debug)]
 pub enum Expr {
-    Assign   (Token, Box<Expr>),
-    Binary   (Box<Expr>, Token, Box<Expr>),
-    Call     (Box<Expr>, Token, Vec<Expr>),
-    Get      (Box<Expr>, Token),
-    Grouping (Box<Expr>),
-    IfExpr   (Box<Expr>, Box<Stmt>, Box<Stmt>),
-    Literal  (Literals),
-    Set      (Box<Expr>, Token, Box<Expr>),
-    Unary    (Token, Box<Expr>),
-    Variable (Token),
+    Assign    (Token, Box<Expr>),
+    Binary    (Box<Expr>, Token, Box<Expr>),
+    Call      (Box<Expr>, Token, Vec<Expr>),
+    Get       (Box<Expr>, Token),
+    Grouping  (Box<Expr>),
+    IfExpr    (Box<Expr>, Box<Stmt>, Box<Stmt>),
+    Index     (Box<Expr>, Token),
+    Literal   (Literals),
+    Set       (Box<Expr>, Token, Box<Expr>),
+    SelfExpr  (Token),
+    SuperExpr (Token, Token),
+    Slice     (Box<Expr>, Token, Token),
+    Unary     (Token, Box<Expr>),
+    Variable  (Token),
 }
