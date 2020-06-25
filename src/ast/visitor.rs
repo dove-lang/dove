da1d6edf20eadd0028fb42060d0d64efd693541c
Peter Ke <peterke0911@gmail.com>
2020-06-24 20:34:37 -0700

    Change result to return Interrupt as error

--- src/ast/visitor.rs
+++ src/ast/visitor.rs
@@ -3,11 +3,11 @@ use crate::ast::{Expr, Stmt};
 pub trait ExprVisitor {
     type Result;
 
-    fn visit_expr(&mut self, expr: &Expr) -> Result<Self::Result, ()>;
+    fn visit_expr(&mut self, expr: &Expr) -> Self::Result;
 }
 
 pub trait StmtVisitor {
     type Result;
 
-    fn visit_stmt(&mut self, stmt: &Stmt) -> Result<(), Self::Result>;
+    fn visit_stmt(&mut self, stmt: &Stmt) -> Self::Result;
 }
